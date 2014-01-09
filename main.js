081b33fff372a0f3450853a2a6b00b93ddaa68e1
garethfoote <gareth.foote@gmail.com>
2014-01-09 11:37:31 +0000

    - Added step animation and glow' - Fixed bug with rendering poems' - Massive restructuring into classes.

--- main.js
+++ main.js
@@ -1,184 +1,422 @@
-var config = {
-    linePadding : 30,
-    wordSpacing : 10
-};
+var app = (function(){
+    "use strict";
 
-var fixtures = {
-    'NN' : [ 'Rock', 'Sea', 'thee', 'Angry', 'power', 'From', 'age', 'Scarred', 'frost']
-};
+    // Private functions.
+    var self = this,
+        config = {
+            linePadding : 30,
+            wordSpacing : 10
+        },
+        wordClasses = ["NNP", "NN", "VBP" ],
+        // wordClasses = ["JJ" ],
+        fixtures = {
+            'NN' : [ 'Rock', 'Sea', 'thee', 'Angry', 'power', 'From', 'age', 'Scarred', 'frost']
+        },
+        paper = {},
+        source = {},
+        target = {},
+        bucketindex = 0,
+        wcindex = 0,
+        cursor = [0, 0],
+        diffX = 0,
+        diffY = 0,
+        swapped = false,
 
-function renderPoem( doc, paper, poem, bucket, startX ){
+        loadPoem = function( source, onLoadCallback ){
 
-    var lines, yPos = 0;
+            var xhr=new XMLHttpRequest();
+            xhr.onload = onLoadCallback;
+            xhr.open("GET", source, false);
+            xhr.send();
 
-    lines = doc.getElementsByTagName('line');
+        },
 
-    for (var i = 0; i < lines.length; i++) {
-        var original = lines[i].getElementsByTagName('original')[0].textContent,
-            tags = lines[i].getElementsByTagName('tag'),
-            tag, path,
-            nextStrIndex, xPos = startX, currStr, hide;
+        init = function(){
 
-        poem[i] = [];
+            paper = Raphael("poem", 900, 1600),
 
-        // Break line into segments on each tag.
-        for (var j = 0; j < tags.length; j++) {
-            hide = false;
+            loadPoem( "dickinson/the-sea.xml", function(){
+                target = new Poem(this.responseXML, 0);
 
-            tag = j>0 ? tags[j-1].getAttribute('class') : "-";
+            });
 
-            nextStrIndex = original.indexOf( tags[j].textContent );
-            currStr = original.substring(0, nextStrIndex);
+            loadPoem( "dickinson/the-rock.xml", function(){
+                source = new Poem(this.responseXML, 450);
+            });
 
-            // TODO - Accomodate for spaces and tabs.
-            if( currStr === " "){
-                // console.log("SPACE");
-                // currStr = "a";
-                hide = true;
-            }
-            if( /\t/.test(currStr) ){
-                // console.log("TAB");
-                // currStr = "ww";
-                hide = true;
-            }
-
-            path = paper.print(
-                            xPos, yPos+50,
-                            currStr,
-                            paper.getFont("Fenix"), 20, 'baseline'
-                        );
-
-            // Remove everything up to this point.
-            original = original.slice( nextStrIndex );
-
-            // Data for replacement.
-            path.data('yPos', yPos+50);
-            path.data('tagClass', tag);
-            path.data('text', currStr);
-            bucket[tag] = bucket[tag] || [];
-            bucket[tag].push(path);
-
-            // Increase xposition and add xspacing.
-            xPos += path.getBBox().width + config.wordSpacing;
-
-            if( hide == true ){
-                // path.hide();
-            }
-
-            poem[i][j] = path;
         };
 
-        // Remainder of the string if there is any.
-        if( original.length > 0 && tags.length > 0 ){
-            tag = tags[j-1].getAttribute('class');
-            poem[i][j] = paper.print(
-                                xPos, yPos+50,
-                                original.substring(0),
-                                paper.getFont("Fenix"), 20, 'baseline');
-            // Data for replacement.
-            path.data('yPos', yPos+50);
-            path.data('tagClass', tag);
-            path.data('text', original.substring(0));
-            bucket[tag] = bucket[tag] || [];
-            bucket[tag].push(path);
-        }
+    // Poem class
+    var Poem = function( doc, startX ){
 
-        if( tags.length === 0 ){
-            yPos += config.linePadding;
+        var lines, yPos = 0,
+            paths = [], bucket = {},
+            lines = doc.getElementsByTagName('line'),
+            cursor = [0,0],
+            bucketindex = 0,
+
+            nexttarget = function( wordclass ){
+
+                var found = false;
+
+                if( gettarget() ){
+                    var tg = gettarget();
+                    console.log(tg.data('text'), tg.getBBox().width);
+                }
+
+                // Loop lines.
+                for (var i = cursor[0]+1; i < paths.length; i++) {
+                    // Break if new word has been found.
+                    if(found){
+                        break;
+                    }
+                    // Loop words in line.
+                    for (var j = 0; j < paths[i].length; j++) {
+                        // Target path is word to be changed.
+                        var targetpath = paths[i][j];
+                        if( targetpath.data('tagClass') == wordclass ){
+                            cursor = [i,j];
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+
+                return targetpath;
+
+            },
+
+            gettarget = function(){
+
+                // No more lines.
+                if( cursor[0] >= paths.length-1 ){
+                    return false;
+                }
+
+                var lineindex = cursor[0],
+                    wordindex = cursor[1];
+
+                return paths[lineindex][wordindex];
+
+            },
+
+            nextsource = function(){
+
+                bucketindex++;
+
+            },
+
+            getsource = function( wordclass ){
+
+                if( ! bucket[wordclass] || bucketindex >= bucket[wordclass].length ){
+                    return false;
+                }
+
+                if( bucket[wordclass][bucketindex].data("transformed") == true ){
+                    console.log("Already transformed");
+                    nextsource();
+                }
+
+                return bucket[wordclass][bucketindex];
+            },
+
+            resetbucket = function(){
+
+                bucketindex = 0;
+
+            },
+
+            resetlines = function(){
+
+                cursor[0] = 0;
+
+            },
+
+            // Shift remaining words in line.
+            shiftwords = function( diffX ){
+
+                var lineindex = cursor[0], wordindex = cursor[1];
+
+                for (var j = wordindex+1; j < paths[lineindex].length; j++) {
+                    // Target path is word to be changed.
+                    var targetpath = paths[lineindex][j];
+                    // If diff has been set then the successive
+                    // words need to be moved by diffX amount.
+                    var newpath = Raphael.transformPath(targetpath.attr("path"), "t"+diffX+",0");
+                    targetpath.animate({ path: newpath},
+                                        1000,
+                                        "linear");
+
+                    // targetpath.animate({transform: "t"+diffX+",0"}, 1000);
+                    // targetpath.transform("T"+diffX+",0");
+                }
+
+            },
+
+            render = function(){
+                // Loop through lines.
+                for (var i = 0; i < lines.length; i++) {
+
+                    var original = lines[i].getElementsByTagName('original')[0].textContent,
+                        tags = lines[i].getElementsByTagName('tag'),
+                        tag, path,
+                        nextStrIndex, xPos = startX, currStr, hide;
+
+                    paths[i] = [];
+
+                    // Break line into segments on each tag.
+                    for (var j = 0; j < tags.length; j++) {
+                        hide = false;
+
+                        // TODO - Explain?
+                        tag = j>0 ? tags[j-1].getAttribute('class') : "-";
+
+                        nextStrIndex = original.indexOf( tags[j].textContent );
+                        currStr = original.substring(0, nextStrIndex);
+                        // console.log(currStr);
+
+                        // TODO - Accomodate for spaces and tabs.
+                        if( currStr === " "){
+                            // console.log("SPACE");
+                            // currStr = "a";
+                            hide = true;
+                        }
+                        if( /\t/.test(currStr) ){
+                            // console.log("TAB");
+                            // currStr = "ww";
+                            hide = true;
+                        }
+
+                        path = paper.print(
+                                        xPos, yPos+50,
+                                        currStr,
+                                        paper.getFont("Fenix"), 20, 'baseline'
+                                    );
+
+                        // Remove everything up to this point.
+                        original = original.slice( nextStrIndex );
+
+                        // Data for replacement.
+                        path.data('yPos', yPos+50);
+                        path.data('tagClass', tag);
+                        path.data('text', currStr);
+                        bucket[tag] = bucket[tag] || [];
+                        bucket[tag].push(path);
+
+                        // Increase xposition and add xspacing.
+                        xPos += path.getBBox().width + config.wordSpacing;
+
+                        if( hide == true ){
+                            // path.hide();
+                        }
+
+                        paths[i][j] = path;
+                    };
+
+                    // Remainder of the string if there is any.
+                    if( original.length > 0 && tags.length > 0 ){
+                        tag = tags[j-1].getAttribute('class');
+                        path = paper.print(
+                                            xPos, yPos+50,
+                                            original.substring(0),
+                                            paper.getFont("Fenix"), 20, 'baseline');
+                        // Data for replacement.
+                        path.data('yPos', yPos+50);
+                        path.data('tagClass', tag);
+                        path.data('text', original.substring(0));
+                        bucket[tag] = bucket[tag] || [];
+                        bucket[tag].push(path);
+                        paths[i][j] = path;
+                    }
+
+                    if( tags.length === 0 ){
+                        yPos += config.linePadding;
+                    } else {
+                        yPos += paths[i][0].getBBox(true).height+config.linePadding;
+                    }
+
+                };
+            };
+
+
+        render();
+
+        return {
+            getpaths    : function(){ return paths },
+            bucket      : bucket,
+            getcursor   : function(){ return cursor },
+            bucketindex : bucketindex,
+            nexttarget  : nexttarget,
+            gettarget   : gettarget,
+            nextsource  : nextsource,
+            getsource   : getsource,
+            shiftwords  : shiftwords,
+            resetlines  : resetlines,
+            resetbucket : resetbucket
+        };
+
+    }; // End Poem()
+
+
+    var next = function(){
+
+        var wc = wordClasses[wcindex];
+
+        var paths = target.getpaths(),
+            bosom = paths[4][3];
+
+        // console.log("Bosom", bosom.getBBox().x);
+
+        target.nexttarget(wc);
+
+        console.log("Line", target.getcursor()[0], "Word", target.getcursor()[1],
+                "Target", !!target.gettarget(), "Source", !! source.getsource(wc));
+
+        // Increment word class if we:
+        // 1. reach the end of the target lines
+        // 2. we reach the end of the source bucket.
+        if( ! target.gettarget()
+                || ! source.getsource(wc) ){
+
+            console.log("Next word class: "+ wordClasses[wcindex+1]);
+            // Next word class.
+            wcindex++;
+            // Reset line.
+            target.resetlines();
+            // Reset bucket.
+            source.resetbucket();
+            if( wcindex >= wordClasses.length ){
+                if( swapped == false ){
+                    console.log("Swap poems.");
+                    swapped = true;
+                    wcindex = 0;
+                    swap();
+                    next();
+                } else {
+                    console.log("Finished.");
+                }
+            } else {
+                next();
+            }
+
         } else {
-            yPos += poem[i][0].getBBox(true).height+config.linePadding;
+            transform();
         }
 
     };
-}
 
+    var completeHandler = function(){
+
+        // Reset diff.
+        diffX = 0;
+        // TODO - Not sure this is the best method. 
+        // Discuss with Mary.
+        // Increment to next available word in bucket.
+        source.nextsource();
+
+        // swap();
+
+        // Pause for ms and then next.
+        setTimeout( next, 500 );
+
+        console.log("+++++++++++++++++++++++++++");
+
+    };
+
+    var transform = function() {
+
+        var wc = wordClasses[wcindex],
+            srcBBox = {}, tgBBox = {};
+
+        // Target path is word to be changed.
+        var targetpath = target.gettarget();
+        tgBBox = targetpath.getBBox();
+
+        // A target word has been found.
+        var sourcepath = source.getsource(wc); 
+        srcBBox = sourcepath.getBBox();
+        console.log("Source", sourcepath.data("text"));
+
+        // Get difference in width in case the sucessive
+        // words in sentence need to be moved.
+        diffX = srcBBox.width - tgBBox.width;
+        diffY = srcBBox.height -tgBBox.height;
+        // console.log("DiffX", diffX);
+        // console.log("DiffY", diffY);
+
+        // Transform target and TODO add callback.
+        transformTarget( targetpath, sourcepath, completeHandler);
+
+        // Indicate the choosen source somehow. Glow or animate.
+        var glow = sourcepath.glow({color: "#fff000"});
+        setTimeout(function(glow){
+            glow.remove();
+        }, 1000, glow);
+
+        // Move rest of line.
+        if( diffX !== 0 ){
+            // diffX -= config.wordSpacing;
+            target.shiftwords(diffX);
+        }
+
+    };
+
+
+    var transformTarget = function( targetpath, sourcepath, completeCallback ){
+
+        // Get path of word (sourcepath) with same wordClass.
+        var bbox = targetpath.getBBox(),
+            newtext = sourcepath.data('text'),
+            newpath = {};
+
+        if( newtext.trim() == ""){
+            // TODO - Why does this have no string?
+            return;
+        }
+
+        // Create new path at same location as target word.
+        // From this we can get the vector path to transform
+        // the target word.
+        newpath = paper.print(
+                    bbox.x, bbox.y+bbox.height,
+                    newtext,
+                    paper.getFont("Fenix"), 20, 'baseline');
+
+        // Animate targetpath path to newpath (based on sourcepath).
+        targetpath.animate({ path: newpath.attr('path')},
+                            1000,
+                            "linear",
+                            completeCallback );
+
+        targetpath.data("transformed", true);
+
+        // Remove new path (was just used as reference).
+        newpath.remove();
+
+    };
+
+    var swap = function(){
+
+        var tmptarget = target;
+
+        target = source;
+        source = tmptarget;
+
+    };
+
+    return {
+        init : init,
+        next : next
+    };
+
+}());
 
 
 window.onload = function () {
-    var paper = Raphael("poem", 900, 600),
-        poem1 = [], poem1Bucket = {},
-        poem2 = [], poem2Bucket = {},
-        startBtn = document.querySelector('.start');
 
-    paper.setViewBox(0, 0, 900, 600);
+    app.init();
 
-    startBtn.onclick = function(){
-
-        var find = document.getElementById("word-class").value,
-            bucketIndex = 0,
-            pause = 500, diffX;
-
-        if( find == "" ){
-            find = "NNP";
-        }
-
-        // Loop lines.
-        for (var i = 0; i < poem1.length; i++) {
-            diffX = 0;
-            // Loop words.
-            for (var j = 0; j < poem1[i].length; j++) {
-                var path = poem1[i][j];
-
-                if( diffX !== 0 ){
-
-                    // If diff has been set then the successive
-                    // words need to be moved by diffX amount.
-                    path.animate({transform: "t"+diffX+",0"}, 1000);
-                    // path.transform("T"+diffX+",0");
-
-                } else if( path.data('tagClass') == find ){
-
-                    var bbox = path.getBBox(),
-                        newtext = poem2Bucket[find][bucketIndex].data('text'),
-                        newpath = {}, diffX = 0;
-
-                    if( newtext.trim() == ""){
-                        // TODO - Why does this have no string?
-                        return;
-                    }
-
-                    // Create new path at same location as word to be replaced.
-                    newpath = paper.print(
-                                bbox.x, bbox.y+bbox.height,
-                                newtext,
-                                paper.getFont("Fenix"), 20, 'baseline');
-                    // Get difference in width.
-                    diffX = newpath.getBBox().width - bbox.width;
-
-                    // Animate old word path to new.
-                    path.animate({ path: newpath.attr('path')}, 1000 );
-
-                    // Remove.
-                    newpath.remove();
-
-                    // TODO - Not sure this is the best method. 
-                    // Discuss with Mary.
-                    bucketIndex++;
-
-                }
-            };
-        };
-
-    };
-
-    /*
-    var letters = paper.print(100, 100, "POETRYROCKS", paper.getFont("Fenix"), 40, 'baseline');
-    letters.attr({fill:"orange"});
-    letters.animate({transform: "t100,100"}, 1000, "<>");
-    */
-
-    xhr=new XMLHttpRequest();
-    xhr.onload = function(){
-        renderPoem( this.responseXML, paper, poem1, poem1Bucket, 0 );
-    };
-    xhr.open("GET","dickinson/the-sea.xml", false);
-    xhr.send();
-
-    xhr=new XMLHttpRequest();
-    xhr.onload = function(){
-        renderPoem( this.responseXML, paper, poem2, poem2Bucket, 450 );
-    };
-    xhr.open("GET","dickinson/the-rock.xml", false);
-    xhr.send();
+    startBtn = document.querySelector('.start');
+    startBtn.onclick = app.next;
 
 };
